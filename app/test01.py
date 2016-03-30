50f91864ed79d276d60b281bc1ec209487c42c87
hosseinkhani <hosseinkhani.farhad@gmail.com>
2016-03-30 22:50:53 +0430

    some gui fixes

--- app/test01.py
+++ app/test01.py
@@ -60,14 +60,23 @@ m = Model(max_turns)
 
 # start stimuli and update the window
 rewards = 0
+reward_img = visual.ImageStim(win=w01, image=DATA_PATH+"/reward.jpg".format(m.state), pos=[0, -6], size=2)
+last_img = None
 while m.remained_turns() > 0:
+    if m.state == 0:
+        last_img = None
+
     # update gui
-    state_imgs = [visual.ImageStim(win=w01, image=DATA_PATH+"/{0}-1.jpg".format(m.state), pos=[-5, 0], size=5),
-                  visual.ImageStim(win=w01, image=DATA_PATH+"/{0}-2.jpg".format(m.state), pos=[5, 0], size=5)]
-    reward_img = visual.TextStim(win=w01, text="cumulative reward: {0}".format(rewards), pos=[-5, 5], color='Black')
+    state_imgs = [visual.ImageStim(win=w01, image=DATA_PATH+"/{0}-1.jpg".format(m.state), pos=[-5, -2], size=5),
+                  visual.ImageStim(win=w01, image=DATA_PATH+"/{0}-2.jpg".format(m.state), pos=[5, -2], size=5)]
+    reward_res = visual.TextStim(win=w01, text="cumulative reward: {0}".format(rewards), pos=[-5, 8], color='Black')
+    turn_res = visual.TextStim(win=w01, text="turn number: {0}".format(m.turn), pos=[-6, 9], color='Black')
     for img in state_imgs:
         img.draw()
-    reward_img.draw()
+    if last_img is not None:
+        last_img.draw()
+    reward_res.draw()
+    turn_res.draw()
     w01.update()
 
     pressed_keys = event.waitKeys(2, ['right', 'left', 'q'])
@@ -85,8 +94,25 @@ while m.remained_turns() > 0:
     else:
         print 'No input, default to right!'
         a = Model.RIGHT
+    if m.state == 0:
+        last_img = state_imgs[0] if a == Model.LEFT else state_imgs[1]
+        last_img.setPos([0, 4])
+
     s, r = m.perform_action(a)
     rewards += r
+
+    # show reward taken
+    if r > 0:
+        reward_res = visual.TextStim(win=w01, text="cumulative reward: {0}".format(rewards), pos=[-5, 8], color='Black')
+        for img in state_imgs:
+            img.draw()
+        last_img.draw()
+        reward_img.draw()
+        reward_res.draw()
+        turn_res.draw()
+        w01.update()
+        core.wait(1)
+
 print "rewards: {0}".format(rewards)
 
 w01.close()
