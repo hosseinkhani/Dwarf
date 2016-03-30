637bdc1625e9ab3eb3fe5189fcfcecb0c83d88a3
hosseinkhani <hosseinkhani.farhad@gmail.com>
2016-03-30 21:26:17 +0430

    minimal daw

--- app/test01.py
+++ app/test01.py
@@ -1,4 +1,52 @@
 from psychopy import visual, core, event
+import numpy as np
+
+
+class Model(object):
+    LEFT = 0
+    RIGHT = 1
+
+    def __init__(self, max_turns):
+        self.max_turns = max_turns
+        self.turn = 0
+        self.state = 0
+
+    def perform_action(self, action):
+        if self.state == 0:
+            if action == self.LEFT:
+                if np.random.uniform() > .7:
+                    self.state = 1
+                    return 1, 0
+                else:
+                    self.state = 2
+                    return 2, 0
+            elif action == self.RIGHT:
+                if np.random.uniform() > .7:
+                    self.state = 2
+                    return 2, 0
+                else:
+                    self.state = 1
+                    return 1, 0
+        elif self.state == 1:
+            self.turn += 1
+            if action == self.LEFT and np.random.uniform() > .8:
+                self.state = 0
+                return 0, 1
+            else:
+                self.state = 0
+                return 0, 0
+        elif self.state == 2:
+            self.turn += 1
+            if action == self.LEFT and np.random.uniform() > .5:
+                self.state = 0
+                return 0, 1
+            else:
+                self.state = 0
+                return 0, 0
+
+    def remained_turns(self):
+        return self.max_turns - self.turn
+
 
 # TODO: read config file
 # init variables
@@ -8,44 +56,36 @@ max_turns = 10
 
 # create a window WTF testMonitor is
 w01 = visual.Window(screen_size, monitor="testMonitor", units="deg", fullscr=True)
-
-# create some stimuli
-# grating = visual.GratingStim(win=w01, mask='circle', size=3, pos=[-4, 0], sf=3)
-# fixation = visual.GratingStim(win=w01, size=0.2, pos=[0, 0], sf=0, rgb=-1)
-
-img11 = visual.ImageStim(win=w01, image="../pics/1-1.jpg", pos=[-10, -5], size=5)
-img12 = visual.ImageStim(win=w01, image="../pics/1-2.jpg", pos=[-10, 5], size=5)
-img21 = visual.ImageStim(win=w01, image="../pics/2-1.jpg", pos=[0, -5], size=5)
-img22 = visual.ImageStim(win=w01, image="../pics/2-2.jpg", pos=[0, 5], size=5)
-img31 = visual.ImageStim(win=w01, image="../pics/3-1.jpg", pos=[10, -5], size=5)
-img32 = visual.ImageStim(win=w01, image="../pics/3-2.jpg", pos=[10, 5], size=5)
+m = Model(max_turns)
 
 # start stimuli and update the window
-state = 1
-turn = 0
-while turn < max_turns:
-    state_imgs = [visual.ImageStim(win=w01, image="../pics/{0}-1.jpg".format(state), pos=[-5, -12 + state * 6], size=5),
-                  visual.ImageStim(win=w01, image="../pics/{0}-2.jpg".format(state), pos=[5, -12 + state * 6], size=5)]
-    if state == 1:
-        state = 2
-    elif state == 2:
-        state = 3
-    elif state == 3:
-        state = 1
-        turn += 1
-    elif state == -1:  # reward phase
-        pass
-
-    # updates
+rewards = 0
+while m.remained_turns() > 0:
+    # update gui
+    state_imgs = [visual.ImageStim(win=w01, image="../pics/{0}-1.jpg".format(m.state), pos=[-5, 0], size=5),
+                  visual.ImageStim(win=w01, image="../pics/{0}-2.jpg".format(m.state), pos=[5, 0], size=5)]
     for img in state_imgs:
         img.draw()
     w01.update()
 
-    if len(event.getKeys()) > 0:
+    core.wait(1)
+
+    pressed_keys = event.getKeys()
+    # print pressed_keys
+    if 'q' in pressed_keys:
         break
     event.clearEvents()
 
-    core.wait(1)
+    if 'left' in pressed_keys:
+        a = Model.LEFT
+    elif 'right' in pressed_keys:
+        a = Model.RIGHT
+    else:
+        print 'No input, default to right!'
+        a = Model.RIGHT
+    s, r = m.perform_action(a)
+    rewards += r
+print "rewards: {0}".format(rewards)
 
 w01.close()
 core.quit()
